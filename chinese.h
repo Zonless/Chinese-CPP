e0f9c12d563d9d58ae4f778e38d966dc3faa61f5
Zonless <118789799+Zonless@users.noreply.github.com>
2024-01-28 10:42:04 +0800

    Upload .h file

---
+++ chinese.h
@@ -0,0 +1,27 @@
+#define 库 <bits/stdc++.h>
+#define 使用 using 
+#define 命名空间 namespace
+#define 标准 std
+#define 整数 int
+#define 浮点数 double
+#define 大整数 long long
+#define 字符串 string
+#define 字符 char
+#define 开关 bool
+#define 输入 cin>>
+#define 与 >>
+#define 输出 cout<<
+#define 和 <<
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 换行 endl 
+#define 有终循环 for
+#define 条件循环 while
+#define 如果 if
+#define 否则 else
+#define 返回 return
+#define 主函数 main()
+#define ； ;
+#define ， ,
